2f54c6bf56f88bbf4bb725064f70f65addde886a
Sidd-harth011 <siddharthgautam573@gmail.com>
2025-09-20 15:18:05 +0530

    not big

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ from PIL import Image
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import io
+import os
 from huggingface_hub import hf_hub_download
+
+# Set Hugging Face cache to a folder inside the container
+os.environ['HF_HUB_CACHE'] = '/app/hf_cache'
 
 # Initialize Flask app
 app = Flask(__name__)
